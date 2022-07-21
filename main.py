8f0570e0dfba9944af1a19401a81046c1b2fca1c
CalvinDabrowski <109753691+CalvinDabrowski@users.noreply.github.com>
2022-07-21 14:40:22 -0500

    Added index page, "item/#" page, backgrounds, cat

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+from starlette.applications import Starlette
+from starlette.routing import Route, Mount
+from starlette.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
 
-app = FastAPI()
 
+templates = Jinja2Templates(directory='templates')
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World, This is Calvin"}
+async def homepage(request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+async def item(request):
+    return templates.TemplateResponse('item.html', {'request': request, 'id' :id})
+
+routes = [
+    Route('/', endpoint=homepage),
+    Route('/item/{id}', endpoint=item),
+    Mount('/static', StaticFiles(directory='static'), name='static')
+]
+
+app = Starlette(debug=True, routes=routes)
