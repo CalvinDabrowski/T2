5dff9bf643b7f4d9bfd9424237952f111e48bc74
CalvinDabrowski <109753691+CalvinDabrowski@users.noreply.github.com>
2022-07-25 19:46:01 -0500

    Added database / json cleaning

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+from pydoc import doc
 from pyexpat import model
 from starlette.routing import Route, Mount
 from starlette.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
 from fastapi import FastAPI, Request
 import requests,json;
+import sqlite3;
+import re;
 
 ## imports
 url : str #used for json
 
-templates = Jinja2Templates(directory='templates')
+templates   = Jinja2Templates(directory='templates')
+connection  = sqlite3.connect('db.sqlite')
+cursor      = connection.cursor()
 
 async def homepage(request):
     return templates.TemplateResponse('index.html', {'request': request}) 
@@ -19,10 +24,8 @@ async def item(request):
 # item handler - applies item.html template to various items. 
 async def apiTest(request):
     return templates.TemplateResponse('apiTesting.html', {'request': request})
-# id does not yet serve a purpose.
 
-
-routes = [
+routes      = [
     Route('/', endpoint=homepage),
     Route('/item/{id}', endpoint=item),
     Mount('/static', StaticFiles(directory='static'), name='static')
@@ -31,17 +34,50 @@ routes = [
 app = FastAPI(debug=True, routes=routes)
 app.route("/apiTesting/{vin}", apiTest)
 
+def checkExist(vinNumber):
+    tables = cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='"+vinNumber+"';").fetchall()
+    if tables == []:
+        print(vinNumber +" not found...")
+        return False
+    else:
+        print("Table found")
+        return True
+# Check if table exists return true/false
+
+def createDB(jsonData, vinNumber):
+    for key, value in jsonData.items():
+        if value:
+            cursor.execute('INSERT INTO '+vinNumber+' VALUES (?,?)', (key, value))
+            print(key + " : " +value + " inserted")
+    connection.commit()
+# Create table w/ vin number (all vins start with A to ensure it's a string)
+
+def getSqlData(vinNumber):
+    with connection:
+        cursor.execute("SELECT * FROM " +vinNumber)
+        print(cursor.fetchall())
+# Print off all data stored in table of vin number
+
 @app.get("/apiTesting/{vin}")
 async def getVIN(request : Request, vin : str):
     url = requests.get("https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/" + vin[0:17] + "?format=json")
-    vinTree = json.loads(json.dumps(url.json()['Results'][0]))
-    model = vinTree['Model']
-    manuf = vinTree['Manufacturer']
-    vinTree['Manufacturer'] =  vinTree['Manufacturer'].replace(",","")
-    vinTree['OtherRestraintSystemInfo'] =  vinTree['OtherRestraintSystemInfo'].replace(",","")
-    mail = vinTree
-    return templates.TemplateResponse('apiTesting.html', {'request': request, "vin" : vin[0:17], "mail" : mail, "model" : model, "manuf" : manuf})
+    importVIN = json.loads(json.dumps(url.json()['Results'][0]))
+    tempDict = ""
+    vinName = "A"+re.sub('[^A-Za-z0-9]+', '', vin[0:17]) # cleans the string prevents injections
+    print(vinName)
+    if not checkExist(vinName):
+        cursor.execute('create table if not exists '+vinName+' (id varchar(2), data json)')
+        createDB(importVIN,vinName)
+        # Checks if a vinnumber is already in the database - prevents multiple entries into database
+    for key, value in importVIN.items():
+        if value:
+            tempDict = tempDict + "<b>" + key + "</b> : " + value + "<br>" 
+        # Format information as a string with the HTML already included. Would look like:  <b> Model </b> : BMW <br> - sends to website
+    model = importVIN['Model']
+    manuf = importVIN['Manufacturer']
+    
+    getSqlData(vinName)
+    return templates.TemplateResponse('apiTesting.html', {'request': request, "vin" : vin[0:17], "mail" : tempDict, "model" : model, "manuf" : manuf})
 # gets a vin number from "vin", passes it thru .get, loads, dumps, and converts it into an array prior to being passed to the html
-
+0
 print("It's recommended you go to the following link:\nhttp://127.0.0.1:8000/")
-
