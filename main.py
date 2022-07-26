4e5769da60f7658e9c43e8f55db44c64d2ad3cdd
CalvinDabrowski <109753691+CalvinDabrowski@users.noreply.github.com>
2022-07-26 13:10:33 -0500

    changed functionality of tables

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI, Request
 import requests,json;
 import sqlite3;
 import re;
+from datetime import datetime
 
 ## imports
 url : str #used for json
@@ -14,7 +15,6 @@ url : str #used for json
 templates   = Jinja2Templates(directory='templates')
 connection  = sqlite3.connect('db.sqlite')
 cursor      = connection.cursor()
-
 
 async def homepage(request):
     return templates.TemplateResponse('index.html', {'request': request}) 
@@ -36,27 +36,21 @@ app = FastAPI(debug=True, routes=routes)
 app.route("/apiTesting/{vin}", apiTest)
 
 def checkExist(vinNumber):
-    tables = cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='"+vinNumber+"';").fetchall()
+    tables = cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='nhtsa_data';").fetchall()
     if tables == []:
-        print(vinNumber +" not found...")
+        print("nhtsa_data not found...")
         return False
     else:
         print("Table found")
         return True
 # Check if table exists return true/false
 
-def createDB(jsonData, vinNumber):
-    for key, value in jsonData.items():
-        if value:
-            cursor.execute('INSERT INTO '+vinNumber+' VALUES (?,?)', (key, value))
-            print(key + " : " +value + " inserted")
+def createDB(vin, jsonData, timeStamp):
+    cursor.execute('INSERT INTO nhtsa_data VALUES (?,?,?)', (vin, jsonData, timeStamp))
+    print("SUCCESS: " + vin + " : " +jsonData + " : " + timeStamp + " inserted")
     connection.commit()
 # Create table w/ vin number (all vins start with A to ensure it's a string)
 
-def getSqlData(vinNumber):
-    with connection:
-        cursor.execute("SELECT * FROM " +vinNumber)
-        print(cursor.fetchall())
 # Print off all data stored in table of vin number
 
 @app.get("/apiTesting/{vin}")
@@ -66,18 +60,16 @@ async def getVIN(request : Request, vin : str):
     tempDict = ""
     vinName = "A"+re.sub('[^A-Za-z0-9]+', '', vin[0:17]) # cleans the string prevents injections
     print(vinName)
-    if not checkExist(vinName):
-        cursor.execute('create table if not exists '+vinName+' (id varchar(2), data json)')
-        createDB(importVIN,vinName)
         # Checks if a vinnumber is already in the database - prevents multiple entries into database
     for key, value in importVIN.items():
         if value:
             tempDict = tempDict + "<b>" + key + "</b> : " + value + "<br>" 
         # Format information as a string with the HTML already included. Would look like:  <b> Model </b> : BMW <br> - sends to website
+    cursor.execute('create table if not exists nhtsa_data  (vin text, response text, created_at timestamp)')
+    createDB(vinName,tempDict, datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
     model = importVIN['Model']
     manuf = importVIN['Manufacturer']
-    
-    getSqlData(vinName)
     return templates.TemplateResponse('apiTesting.html', {'request': request, "vin" : vin[0:17], "mail" : tempDict, "model" : model, "manuf" : manuf})
 # gets a vin number from "vin", passes it thru .get, loads, dumps, and converts it into an array prior to being passed to the html
+
 print("It's recommended you go to the following link:\nhttp://127.0.0.1:8000/")
